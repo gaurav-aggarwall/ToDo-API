a9ddcb81c0ffe85be0253b19d6cc921bb582955a
gaurav1209 <aggarwal.gaurav10@gmail.com>
2019-02-15 01:07:12 +0530

    Deploying to Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+
+const URI = "mongodb://TEST_USER_1:akku2223@todo-api-shard-00-00-brcin.mongodb.net:27017,todo-api-shard-00-01-brcin.mongodb.net:27017,todo-api-shard-00-02-brcin.mongodb.net:27017/test?ssl=true&replicaSet=ToDo-API-shard-0&authSource=admin&retryWrites=true";
+
+mongoose.connect(URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 module.exports = {mongoose};
